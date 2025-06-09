d76eff9a6b0d883cddc088e41e69608e0c4d0d8c
Nyx <xamarynth@gmail.com>
2025-06-08 20:28:33 -0400

    Week 2: Python Quote

---
+++ week-2/quote.py
@@ -0,0 +1,6 @@
+# Web 335
+# Date: 06/08/2025
+# File Name: quote.py
+# Description: A simple python program to print a nice quote
+
+print("'The Capacity to learn in a gift; The ability to learn is a skill; The willingness to learn is a choice.' - Quote by Brian Herbert")
