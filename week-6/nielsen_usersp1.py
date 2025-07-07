24aea11b1a5bdee738b31ad600c54826dd315037
Nyx <xamarynth@gmail.com>
2025-07-07 18:31:52 -0400

    Week 6: Hands-On 4.2

--- week-6/nielsen_usersp1.py
+++ week-6/nielsen_usersp1.py
@@ -7,8 +7,8 @@
 from pymongo import MongoClient
 
 # Step 1: Connect to MongoDB Atlas cluster
-client = MongoClient("mongodb+srv://web355Admin:<password>@bellevueuniversity.a2hoz8c.mongodb.net/web335DB?retryWrites=true&w=majority")
-db = client['BellevueUniversity']
+client = MongoClient("mongodb+srv://web355Admin:s3cret@bellevueuniversity.a2hoz8c.mongodb.net/web335DB?retryWrites=true&w=majority")
+db = client['BellevueUniversity'] 
 
 # Step 2: Display all documents in the user's collection (showing first and last names)
 print("\n All users (first and last names):")
